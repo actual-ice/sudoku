9b2ec7fee3de9a4c4f9e117152ceda1a5bfb9d94
actual-ice <isaiah.penullar@gmail.com>
2021-06-16 20:50:40 +0800

    making boilerplate code, created class

---
+++ sudoku.py
@@ -0,0 +1,14 @@
+class sudoku_game():
+    def __init__(self):
+        self.__gameboard = random_game() # generate a list of numbers for a valid sudoku game
+        main_loop() # start the game
+
+    def make_move(self):
+        move = get_move() # move = (xpos, ypos, move_input)
+        if legal_move(move):
+            self.__gameboard[move[0], move[1]] = move[2]
+
+    def __main_loop():
+        
+
+    
